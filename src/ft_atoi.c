d2fc701a8e5cb9c90dcfc5272e9738d8af3eb7df
TheGloomist <woodchps.picture@gmail.com>
2025-05-20 20:49:17 +0200

    Atoi. Not normed, tested

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -2,5 +2,24 @@
 
 int	ft_atoi(const char *nptr)
 {
-	
+	size_t		i;
+	int		sign;
+	int		conv;
+
+	i = 0;
+	sign = 1;
+	conv = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t' || \
+	nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '-')
+		sign = -1;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		i++;
+	while (ft_isdigit(nptr[i]))
+	{
+		conv = (conv * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (conv * sign);
 }
